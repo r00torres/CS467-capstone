c82f200ba69b2abbd304ae21a27fac20377841fb
Ruben M Torres <rumitorres@gmail.com>
2019-05-11 20:52:18 -0700

    Update save.js

--- nodetest/public/save.js
+++ nodetest/public/save.js
@@ -1,22 +1,5 @@
 var savedTowers = [];
 
-//gotta save the currently built towers! web suggest convert to json and create cookie this way
-/*function saveTowerLocations( endOfWaveTowers[] ) {
-  for (var i = 0; i < endOfWaveTowers.length; i++){
-    savedTowers.push({endOfWaveTowers[i].position.x, endOfWaveTowers[i].name});
-  }
-  
-  //https://stackoverflow.com/questions/2980143/i-want-to-store-javascript-array-as-a-cookie
-  var json_str = JSON.stringify(savedTowers);
-  
-  setCookie('loadBuildMap', json_str);
-  
-}*/
-
-//this will require that an additional function thatcan create these towers on the board upon load
-
-//after each wave or level do we update? we would need to make this clear to user
-
 function setCookie(cname, cvalue, exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -40,6 +23,19 @@ function getCookie(cname) {
   return "";
 }
 
+//gotta save the currently built towers! web suggest convert to json and create cookie this way
+function saveTowerLocations( towers ) {
+  for (var i = 0; i < towers.length; i++){
+    savedTowers.push({towers[i].position, towers[i].name});
+  }
+  
+  //https://stackoverflow.com/questions/2980143/i-want-to-store-javascript-array-as-a-cookie
+  var json_str = JSON.stringify(savedTowers);
+  
+  setCookie('loadBuildMap', json_str);
+  
+}
+
 function checkCookie() {
   var username = getCookie("username");
   if (username != "") {
@@ -53,12 +49,14 @@ function checkCookie() {
     }
   }
 }
+
+//testing saving tower pos and name
 let position = {
                 x: 1,
                 y: 1,
                 z: 1,
               }
-//testing saving tower pos and name
+
 savedTowers.push({ position, name:"test" });
 savedTowers.push({ position, name:"test1" });
 savedTowers.push({ position, name:"test2" });
