66b18e2b7def02edc31493023ca8a8283fc11f0c
cfrenchi <christopher.frenchi@gmail.com>
2019-05-20 22:35:22 -0400

    w7

--- nodetest/public/rules.js
+++ nodetest/public/rules.js
@@ -9,12 +9,15 @@ function mapWaves(curWave){
   console.log("MAPWAVES******************");
   console.log("wt txw clock", waveTime, timeXwave, clock.elapsedTime);
   updateGameUIbar("waves", curWave);
+  if(curWave == totWaves){
+    console.log("You win!");
+  }
   if( curWave < totWaves && lives > 0){
     console.log("mapwaves clock", clock.elapsedTime);
     var delay = 0;
     //delay += 10000;
 
-    for(var i=0; i<3; i++)
+    for(var i=0; i<curWave; i++)
     {
       addOviGLTF(scene, dinos, delay);
       delay += 500;
@@ -70,4 +73,26 @@ function mapWaves(curWave){
 //Win lose conditions
 function loseCondition(){
   console.log("YOU LOSE", lives);
+  //https://stackoverflow.com/questions/30359830/how-do-i-clear-three-js-scene
+  while ( scene.children.length > 0 ) {
+     scene.children.pop();
+  }
+  console.log(scene);
+  clearErrythang();
+}
+
+function clearErrythang() {
+  // var animationID = requestAnimationFrame(animate);
+  // cancelAnimationFrame(animationID);
+  // scene = NULL;
+  // camera = NULL;
+  // renderer = NULL;
+  // controls = NULL;
+  // stats = NULL;
+  // raycaster = NULL;
+  // mouse = NULL;
+  // setTimeout( function(){
+  //   init();
+  // }, 500);
+  menu();
 }
