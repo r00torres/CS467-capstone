d3249c553b0b8e2960b0754242176d4daa1e7b05
Ruben M Torres <torrerub@oregonstate.edu>
2019-05-25 07:06:09 -0700

    Update load.js

--- nodetest/public/load.js
+++ nodetest/public/load.js
@@ -82,7 +82,7 @@ function loadGame() {
 //checks to see if a buildable space already has a tower
 //false - okay to build
 //true - tower was loaded at this position, change space to buildable on gridT
-funtion checkFromLoaded(intersection, towers){
+function checkFromLoaded(intersection, towers){
   
   //nothing loaded, good to build
   if(towers.length < 0){
